60c49dd33d7e83f2bea4d7b768be5ff1221a2bbb
mini-song <ajohn96@naver.com>
2021-02-24 14:37:42 +0900

    2021-02-24

--- 크롤링 연습/만개의 레시피.py
+++ 크롤링 연습/만개의 레시피.py
@@ -14,6 +14,19 @@
 #     name: python3
 # ---
 
+# +
+qwe =[['ab'],['b'],['c','a'],['d']]
+
+for i in qwe:
+    for j in i:
+        if 'a' in j:
+            print('포함',j)
+        elif 'b' in j:
+            print("포함",j)
+        else:
+            print('미포함',j)
+# -
+
 import requests
 from bs4 import BeautifulSoup
 from selenium import webdriver
@@ -122,7 +135,7 @@ import pandas as pd
 # +
 url_1000개=[]
 
-for i in tqdm(range(1,11)):
+for i in tqdm(range(1,26)):
     try:
         url = 'https://www.10000recipe.com/recipe/list.html?cat3=70&order=reco&page='+str(i)
         req = requests.get(url)
@@ -137,22 +150,20 @@ for i in tqdm(range(1,11)):
         pass
 
 # +
-url = 'https://www.10000recipe.com/recipe/list.html?cat3=70&order=reco&page=1'
-req = requests.get(url)
-html = req.text
-soup = BeautifulSoup(html,'html.parser')
-소고기_number_list=soup.find_all("div",attrs={'class':'common_sp_thumb'})
+#test_=url_1000개[1:100]
 
-browser.quit()
-# -
-
-len(url_1000개)
-
-url_1000개[230]
+# +
+#https://www.10000recipe.com/recipe/6883714
 
 # +
 
-csv_list=[['url','title','재료','태그']]
+
+import re
+testt = ['https://www.10000recipe.com/recipe/6852019']
+csv_list=[['title','재료','태그','url','이미지주소']]
+
+
+
 for url_ in tqdm(url_1000개):
     
     url = url_
@@ -176,19 +187,119 @@ for url_ in tqdm(url_1000개):
             tag.append(i.text.replace('#',''))
     except Exception:
         pass
-    for i in t:
-        재료.append(i.text.strip().replace(" ","").replace("\n"," "))
-    하나.append(url)
-    하나=[]
-    하나.append(title)
-    하나.append(재료)
-    하나.append(tag)
-    csv_list.append(하나)
+    스위치=0
+    for i in tag:
+    
+        if '자취' in i:
+            print(i)
+            스위치=스위치+1
+        elif '간단' in i:
+            print(i)
+            스위치=스위치+1
+        else:
+            pass
+
+    
+    if 스위치 >=1:
+        for i in t:
+            재료.append(i.text.strip().replace("\n","").split(" ")[0])
+        
+        하나=[]
+        image = soup.find("div",attrs={'class':'centeredcrop'}).find('img')['src']
+        하나.append(title)
+        하나.append(재료)
+        하나.append(tag)
+        하나.append(url)
+        하나.append(image)
+        csv_list.append(하나)
+    else:
+        pass
+
+# -
+
+csv_list
+
 # +
 import pandas as pd
 
 바꾸자=pd.DataFrame(csv_list)
-바꾸자.to_csv('웹스크래핑_400_2.csv',encoding='utf-8-sig',header=False)
+바꾸자.to_csv('웹스크래핑_0224.csv',encoding='utf-8-sig',header=False)
+
+
+# +
+url = 'https://www.10000recipe.com/recipe/list.html?cat3=70&order=reco&page=1'
+req = requests.get(url)
+html = req.text
+soup = BeautifulSoup(html,'html.parser')
+소고기_number_list=soup.find_all("div",attrs={'class':'common_sp_thumb'})
+
+browser.quit()
+# -
+
+len(url_1000개)
+
+len(url_1000개)
+
+# +
+import re
+csv_list=[['title','재료','태그','url']]
+
+
+
+for url_ in tqdm(url_1000개):
+    
+    url = url_
+    req = requests.get(url)
+    html = req.text
+    soup = BeautifulSoup(html,'html.parser')
+    #movies = soup.find("div",attrs={'class':'view2_summary st3'})
+    try:
+        ingredient = soup.find("div",attrs={'class':'ready_ingre3'})
+    except Exception:
+        pass
+    browser.quit()
+    재료=[]
+    title=soup.find("h3").text
+    try:
+        t=ingredient.find_all('a')
+    
+        s=soup.find("div", attrs={'class':"view_tag"})
+        tag=[]
+        for i in s:
+            tag.append(i.text.replace('#',''))
+    except Exception:
+        pass
+    스위치=0
+    for i in tag:
+    
+        if '자취' in i:
+            print(i)
+            스위치=스위치+1
+        elif '간단' in i:
+            print(i)
+            스위치=스위치+1
+        else:
+            pass
+
+    
+    if 스위치 >=1:
+        for i in t:
+            재료.append(i.text.strip().replace("\n","").split(" ")[0])
+        
+        하나=[]
+        
+        하나.append(title)
+        하나.append(재료)
+        하나.append(tag)
+        하나.append(url)
+        csv_list.append(하나)
+    else:
+        pass
+# +
+import pandas as pd
+
+바꾸자=pd.DataFrame(csv_list)
+바꾸자.to_csv('웹스크래핑_1000개 필터.csv',encoding='utf-8-sig',header=False)
 
 
 
